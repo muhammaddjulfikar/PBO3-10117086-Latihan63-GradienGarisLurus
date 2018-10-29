701469c103e241469af98fea14bf020a38926f34
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-29 23:26:31 +0700

    PBO3-10117086-Latihan63-GradienGarisLurus

---
+++ src/pbo3/pkg10117086/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface GarisLurus {
+    
+   public int hitungGradien();
+    
+}
+
